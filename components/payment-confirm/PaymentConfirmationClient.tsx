e690c41bd314cb2a085e12aa7982e986945006e6
huntdavid175 <huntdavid175@gmail.com>
2025-07-09 20:31:04 -0300

    changed the text size of the title of the payment success

--- components/payment-confirm/PaymentConfirmationClient.tsx
+++ components/payment-confirm/PaymentConfirmationClient.tsx
@@ -155,7 +155,9 @@ const PaymentConfirmationClient = ({ orderDetails }: { orderDetails: any }) => {
             <div className="inline-flex items-center justify-center w-16 h-16 bg-green-100 rounded-full mb-4">
               <CheckCircle className="h-8 w-8 text-green-600" />
             </div>
-            <h1 className="text-3xl font-bold mb-2">Payment Successful!</h1>
+            <h1 className="text-xl md:text-3xl font-bold mb-2">
+              Payment Successful!
+            </h1>
             <p className="text-gray-600 mb-6 text-xs md:text-sm">
               Thank you for your order. Your payment has been processed
               successfully.
